42af42531563eab3b751c13caaa7fc3c0ac81c70
Morpa <andremorpa@gmail.com>
2026-07-19 14:14:54 +0100

    add cursorHold

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -9,3 +9,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
     end,
 })
+
+
+vim.o.updatetime = 300 -- ms até disparar CursorHold
+
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.lsp.buf.hover()
+    end,
+})
